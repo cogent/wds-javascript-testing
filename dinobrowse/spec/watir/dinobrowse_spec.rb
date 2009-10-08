1231a8524a05b892f04c9d203875d2829254282f
Rob Mitchell <rob.mitchell@cogentconsulting.com.au>
2009-10-08 22:36:37 +1100

    Some more watir tests

--- dinobrowse/spec/watir/dinobrowse_spec.rb
+++ dinobrowse/spec/watir/dinobrowse_spec.rb
@@ -7,8 +7,18 @@ describe "DinoBrowse" do
     $browser.goto("http://localhost:4444")
   end
   
-  it %(has the title "Jonah's Dinosaurs") do
-    $browser.title.should == %(Jonah's Dinosaurs)
+  it "has the title Dinos-R-us" do
+    $browser.title.should == "Dinos-R-us"
   end
   
+  describe "select a dinosaur" do
+    before do
+      $browser.link(:text, 'Apatosaurus').click
+    end
+    
+    it "should show dinosaur content" do
+      $browser.div(:id, 'apatosaurus')
+    end
+    
+  end
 end
